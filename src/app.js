aa900c4c305a5e5842997cc41c24266b40a3fe2c
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2021-11-28 00:14:15 +0200

    divide shape-module to animation module, add few functions t o

--- src/app.js
+++ src/app.js
@@ -2,30 +2,41 @@ import './styles.css';
 import {
   ContextMenu
 } from './menu';
-import {
-  SoundModule
-} from './modules/sound.module';
+import {SoundModule} from './modules/sound.module';
+import {Timer} from './modules/timer.module';
+import {ShapeModule} from './modules/shape.module';
+import {BackgroundModule} from './modules/background.module';
 
 const menu = new ContextMenu('#menu');
 
-const modules = [new SoundModule('sound', 'Случайный звук'), new SoundModule('sound', 'Boom')];
+const modules = [
+  new SoundModule('sound', 'Рандомный звук'),
+  new Timer('timer', 'Таймер'),
+  new ShapeModule('shape', 'Случайная фигура'),
+  new BackgroundModule('background', 'Случайный фон'),
+];
 
 menu.add(modules);
 
-document.body.addEventListener('contextmenu', (e) => {
-  e.preventDefault();
-  const mouseX = e.pageX;
-  const mouseY = e.pageY;
-
-  menu.open(mouseX, mouseY);
-
-
-  menu.el.addEventListener('click', (e) => {
-    const moduleType = e.target.dataset.type;
-    modules.forEach(item => {
-      if (item.type === moduleType) {
-        item.trigger();
-      }
-    });
+function triggerModule(e) {
+  const moduleType = e.target.dataset.type;
+  modules.forEach(item => {
+    if (item.type === moduleType) {
+      item.trigger(e);
+      menu.el.removeEventListener('click', triggerModule);
+      menu.close();
+    }
   });
-});
+}
+
+function showContextMenu(event) {
+  event.preventDefault();
+  const mouseX = event.pageX;
+  const mouseY = event.pageY;
+  menu.open(mouseX, mouseY);
+  menu.el.addEventListener('click', triggerModule);
+  document.body.removeEventListener('click', triggerModule);
+}
+
+document.body.addEventListener('contextmenu', showContextMenu);
+
