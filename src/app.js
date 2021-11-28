742b611ecdb62fb8c97a768e5e16e1be8c29c976
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2021-11-28 14:53:34 +0200

    reworked shape module

--- src/app.js
+++ src/app.js
@@ -5,27 +5,67 @@ import {
 import {
   SoundModule
 } from './modules/sound.module';
+import {
+  Timer
+} from './modules/timer.module';
+import {
+  ShapeModule
+} from './modules/shape.module';
+import {
+  BackgroundModule
+} from './modules/background.module';
+import {
+  RandomtextModule
+} from './modules/randomtext.module';
+import {
+  ClicksModule
+} from './modules/clicks.module';
 
 const menu = new ContextMenu('#menu');
 
-const modules = [new SoundModule('sound', 'Случайный звук'), new SoundModule('sound', 'Boom')];
+const modules = [
+  new SoundModule('sound', 'Рандомный звук'),
+  new Timer('timer', 'Таймер'),
+  new ShapeModule('shape', 'Случайная фигура'),
+  new BackgroundModule('background', 'Случайный фон'),
+  new RandomtextModule('randomtext', 'Случайная цитата'),
+  new ClicksModule(),
+];
 
-menu.add(modules);
+class App {
+  constructor(modules, menu) {
+    this.modules = modules;
+    this.menu = menu;
+  }
 
-document.body.addEventListener('contextmenu', (e) => {
-  e.preventDefault();
-  const mouseX = e.pageX;
-  const mouseY = e.pageY;
+  showContextMenu(event) {
+    event.preventDefault();
+    const mouseX = event.pageX;
+    const mouseY = event.pageY;
+    const appMenu = this.menu;
 
-  menu.open(mouseX, mouseY);
+    function triggerModule(e) {
+      const moduleType = e.target.dataset.type;
+      modules.forEach(item => {
+        if (item.type === moduleType) {
+          item.trigger(e);
+          appMenu.close();
+          appMenu.removeListener('click', triggerModule);
+        }
+      });
+    }
 
+    this.menu.open(mouseX, mouseY);
+    this.menu.el.addEventListener('click', triggerModule);
 
-  menu.el.addEventListener('click', (e) => {
-    const moduleType = e.target.dataset.type;
-    modules.forEach(item => {
-      if (item.type === moduleType) {
-        item.trigger();
-      }
-    });
-  });
-});
+  }
+  run() {
+    this.menu.add(this.modules);
+    const bindedContextMenu = this.showContextMenu.bind(this);
+    document.body.addEventListener('contextmenu', bindedContextMenu);
+  }
+}
+
+const app = new App(modules, menu);
+
+app.run();
